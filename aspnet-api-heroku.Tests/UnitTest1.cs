91a188083038532491f1ddb2791cff42af742ca2
Richard Poulson <contact@richardpoulson.com>
2020-05-22 10:38:34 -0600

    Configuring MSTest project for unit testing

---
+++ aspnet-api-heroku.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace aspnet_api_heroku.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
