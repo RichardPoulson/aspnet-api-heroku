ca251e7811d58685eca9334b39fef51c2ef64cb4
Richard Poulson <contact@richardpoulson.com>
2020-06-01 18:45:19 -0600

    Added xUnit and MSTest unit tests

---
+++ aspnet-api-heroku.MSTest/UnitTest1.cs
@@ -0,0 +1,69 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace aspnet_api_heroku.MSTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private static TestServer _server;
+        private static HttpClient _client;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            // Arrange
+            _server = new TestServer(new WebHostBuilder()
+               .UseStartup<Startup>());
+            _client = _server.CreateClient();
+        }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            // Runs before each test. (Optional)
+        }
+
+        [AssemblyCleanup]
+        public static void AssemblyCleanup()
+        {
+            // Executes once after the test run. (Optional)
+        }
+
+        [ClassCleanup]
+        public static void TestFixtureTearDown()
+        {
+            // Runs once after all tests in this class are executed. (Optional)
+            // Not guaranteed that it executes instantly after all tests from the class.
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            // Runs after each test. (Optional)
+        }
+
+        [TestMethod]
+        public async Task ArticleItems_SuccessStatusCode()
+        {
+            // Act
+            var response = await _client.GetAsync("api/ArticleItems");
+            // Assert
+            response.EnsureSuccessStatusCode();
+            //var responseString = await response.Content.ReadAsStringAsync();
+            //Assert.Equal("[]", responseString);
+        }
+
+        [TestMethod]
+        public async Task Swagger_SuccessStatusCode()
+        {
+            // Act
+            var response = await _client.GetAsync("swagger/index.html");
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
---
+++ aspnet-api-heroku.xUnit/UnitTest1.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Xunit;
+
+namespace aspnet_api_heroku.xUnit
+{
+    public class IntegrationTest1
+    {
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+
+        public IntegrationTest1()
+        {
+            // Arrange
+            _server = new TestServer(new WebHostBuilder()
+               .UseStartup<Startup>());
+            _client = _server.CreateClient();
+        }
+
+        [Fact]
+        public async Task ArticleItems_SuccessStatusCode()
+        {
+            // Act
+            var response = await _client.GetAsync("api/ArticleItems");
+            // Assert
+            response.EnsureSuccessStatusCode();
+            //var responseString = await response.Content.ReadAsStringAsync();
+            //Assert.Equal("[]", responseString);
+        }
+
+        [Fact]
+        public async Task Swagger_SuccessStatusCode()
+        {
+            // Act
+            var response = await _client.GetAsync("swagger/index.html");
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
